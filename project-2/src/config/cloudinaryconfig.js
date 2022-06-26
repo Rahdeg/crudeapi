338605d4a7e878b84be82d60b971b91755d93912
Rahdeg <walett95@gmail.com>
2022-06-26 19:23:38 +0100

    ss

--- project-2/src/config/cloudinaryconfig.js
+++ project-2/src/config/cloudinaryconfig.js
@@ -1,10 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
-
 cloudinary.config({
-cloud_name: "djdyxth0z",
-api_key: "562539624165535",
-api_secret: "FWx6EfPUFehNmLcOxYhEftYxQjY",
+cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+api_key: process.env.CLOUDINARY_API_KEY,
+api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 
